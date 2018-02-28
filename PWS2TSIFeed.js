2c69b6552179de6ce49c738b7701b13a41ae2477
Bobby McCullough <32373236+bobbymccullough@users.noreply.github.com>
2018-02-28 14:06:39 -0600

    Create PWS2TSIFeed.js

---
+++ PWS2TSIFeed.js
@@ -0,0 +1,183 @@
+//establish the Ambient Weather API Environment and ensures all needed libraries are available
+require('dotenv').config()
+const AmbientWeatherApi = require('../lib/index')
+
+// helper function
+function getName (device) {
+  return device.info.name
+}
+
+//establishes the Ambient Weather API Key and Application Key
+const apiKey = process.env.AMBIENT_WEATHER_API_KEY || '<ENTER AMBIENT WEATHER API KEY HERE>'
+const api1 = new AmbientWeatherApi({
+  apiKey,
+  applicationKey: process.env.AMBIENT_WEATHER_APPLICATION_KEY || '<ENTER AMBIENT WEATHER APPLICATION KEY HERE>'
+})
+
+//establishes a connection to Ambient weather and reports back that its done so
+api1.connect()
+api1.on('connect', () => console.log('Connected to Ambient Weather Realtime API!'))
+
+//shows how many devices are subscribed and their names
+api1.on('subscribed', data => {
+  console.log('Subscribed to ' + data.devices.length + ' device(s): ')
+  console.log(data.devices.map(getName).join(', '))
+})
+
+//when data is received from the connection, it is output to the console...
+//using the layout below.  The POST portion of the program is also called...
+//whereby a connection to TrueSight is established so that the data array...
+//can be posted.
+api1.on('data', data => {
+  console.log("UTC DateTime  :  " + data.dateutc)
+  console.log("PWS           :  " + getName(data.device))
+  console.log("Temperature   :  " + data.tempf)
+  console.log("Feels Like    :  " + data.feelsLike)
+  console.log("Humidity      :  " + data.humidity)
+  console.log("Barometer     :  " + data.baromrelin)
+  console.log("Hourly Rain   :  " + data.hourlyrainin)
+  console.log("Daily Rain    :  " + data.dailyrainin)
+  console.log("Monthly Rain  :  " + data.monthlyrainin)
+  console.log("Wind Direction:  " + data.winddir)
+  console.log("Wind Speed    :  " + data.windspeedmph)
+  console.log("Wind Gust     :  " + data.windgustmph)
+  console.log("Max Daily Gust:  " + data.maxdailygust)
+  //console.log(data.yearlyrainin) - not available
+  //console.log(data.windgustdir) - not available
+
+//This begins the "POST" portion of the program
+  const request = require('request')
+
+//Establishes the POST header
+  const headers2 = {
+      'Content-Type': 'application/json'
+  }
+
+//Establishes the data array that will be sent to TrueSight Intelligence...
+//This section can be modified to include only those metrics you wish.
+  const vdataArray = [
+       [
+          "api.ambientweather.net",
+          "tempf",
+          data.tempf,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],
+       [
+          "api.ambientweather.net",
+          "feelsLike",
+          data.feelsLike,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],[
+          "api.ambientweather.net",
+          "humidity",
+          data.humidity,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],
+       [
+          "api.ambientweather.net",
+          "baromrelin",
+          data.baromrelin,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],
+       [
+          "api.ambientweather.net",
+          "hourlyrainin",
+          data.hourlyrainin,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],
+       [
+          "api.ambientweather.net",
+          "dailyrainin",
+          data.dailyrainin,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],
+       [
+          "api.ambientweather.net",
+          "weeklyrainin",
+          data.weeklyrainin,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],
+       [
+          "api.ambientweather.net",
+          "monthlyrainin",
+          data.monthlyrainin,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],
+       [
+          "api.ambientweather.net",
+          "winddir",
+          data.winddir,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],
+       [
+          "api.ambientweather.net",
+          "windspeedmph",
+          data.windspeedmph,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],
+       [
+          "api.ambientweather.net",
+          "windgustmph",
+          data.windgustmph,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],
+       [
+          "api.ambientweather.net",
+          "maxdailygust",
+          data.maxdailygust,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],
+       [
+          "api.ambientweather.net",
+          "uv",
+          data.uv,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ],
+       [
+          "api.ambientweather.net",
+          "solarradiation",
+          data.solarradiation,
+          data.dateutc,
+          {"app_id":getName(data.device)}
+       ]
+    ]
+
+//Establishes the parameters for the POST action
+  var options = {
+    url: 'https://api.truesight.bmc.com/v1/measurements',
+    method: 'POST',
+    headers: headers2,
+    body: vdataArray,
+    json: true,
+    auth: {
+      'user': '<ENTER TRUESIGHT INTELLIGENCE LOGIN HERE>',
+      'pass': '<ENTER TRUESIGHT INTELLIGENCE API TOKEN HERE>'
+      }
+  }
+
+//Establishes the callback function and how an error is handled
+function callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+          console.log(body)
+      }
+  }
+
+//POST primary action that adds a measurement(s) to TrueSight
+request(options, callback)
+});
+
+//Establishes the Ambient Weather subscription
+api1.subscribe(apiKey)
